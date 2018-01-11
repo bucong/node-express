95c07930c8b3380adc37f59b3bc3f6acd270004f
bucong <bucong@sodo-tech.com>
2018-01-11 11:03:45 +0800

    本地图片上传

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,33 @@ var connection = require('./db');
 var fs = require('fs');
 var multiparty = require('connect-multiparty');
 
+//上传图片接口
+router.post('/upload_img',multiparty(),function(req, res,next) {
+    var posterData = req.files.imgFile;
+    var filePath = posterData.path;
+    var originalFilename = posterData.originalFilename;
+    if (originalFilename) {
+        fs.readFile(filePath, function(err, data) {
+            var timestamp = Date.now();//当前时间戳
+            var type = posterData.type.split('/')[1];//图片类型
+            var posterUrl = 'images/'+timestamp + '.' + type;//回发路径和名称
+            var poster = 'public/images/'+timestamp + '.' + type;//图片保存路径和名称
+            fs.writeFile(poster, data, function(err) {
+                if(err)
+                    throw err;
+                console.log('写入图片成功');
+                //回发图片路径
+                res.send({
+                    errno: 0,
+                    data:[posterUrl]
+                })
+            })
+        })
+    }else{
+        res.send('上传失败');
+    }
+});
+
 //获取轮播图
 router.get('/banner',(req,res)=>{
     console.log('轮播图');
@@ -14,7 +41,7 @@ router.get('/banner',(req,res)=>{
         })
     })
 });
-
+//上传轮播图
 router.post('/banner_set',multiparty(),function(req, res,next) {
     var posterData = req.files.imgFile;
     var filePath = posterData.path;
